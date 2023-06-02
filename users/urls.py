0bf60602d9086202d6021f6c864063d7e94b5d23
Kevin-Kiprotich <kevinkiprotich0089@gmail.com>
2023-06-02 09:11:24 +0300

    Added URLs

---
+++ users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from django.conf import settings
+from . import views
+
+urlpatterns = [
+    path('login/', views.LoginView.as_view()),
+    path('signup/',views.SignUpView.as_view()),
+]
