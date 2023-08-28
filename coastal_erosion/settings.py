7ec68db397c1901b45a1a819bb76abed70adb645
Kevin-Kiprotich <kevinkiprotich0089@gmail.com>
2023-08-28 20:09:16 +0300

    User Statistics dashboard

--- coastal_erosion/settings.py
+++ coastal_erosion/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-79u!%+z7v!v@hy3_f!o1nc!&!%jyybck8kd6q-l_y16f1ez1hi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
